59c939701d2ea672c7a44c71e4ec1a6c8928f667
ET-stack <1826869334@qq.com>
2021-10-10 19:42:06 +0800

    换成自己的

--- assets/js/tongji.baidu.js
+++ assets/js/tongji.baidu.js
@@ -1,7 +1,9 @@
+<script>
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?ec7470452f344e57c8085be5be58583f";
-  var s = document.getElementsByTagName("script")[0];
+  hm.src = "https://hm.baidu.com/hm.js?5d7bb93974444255e9d24b664e43b503";
+  var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
+</script>
